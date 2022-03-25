b76222ef5b393daa9c7791819a056f347b2664ab
minhtam-may <84617312+minhtam-may@users.noreply.github.com>
2022-03-25 21:57:16 +0700

    minh
    toi vua tai len

---
+++ denthongminh.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class thoi_Tiet
+{
+	protected:
+		int humid;
+		int cur_humid==0;
+	public:
+		void nhapvaodoam()
+		{cout<< "\nNhap do am hien tai: " ; cin>> cur_humid;}
+		void inradoamhientai()
+		{
+		 showthoi_Tiet();
+		 cout<<endl<<"Do am hien tai la: "<< cur_humid<<"%";
+		 bool check = true ;
+		 while(check)
+		 {
+		 	if (cur_humid> humid)
+		 {
+		 	cout <<endl<<"Do am hien tai lon -Can tang  them do sang";
+		 	check=false;
+		 }else
+		{
+			cout<<"\nKhong can tang  them do sang ";
+			break;
+		}
+	}
+		
+}
+        void showthoi_Tiet()
+        {
+        	cout<<endl<<"Do am ly tuong : "<< humid<< "%";
+		}
+};
